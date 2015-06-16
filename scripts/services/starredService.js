f37cfdd2387d35efd142f7e955f81d076fb431d7
Joan Biscarri <joan@biscarri.com>
2015-06-16 21:45:14 +0200

    Reparado error con indexOf para encontrar un elemento en un array de ints

--- scripts/services/starredService.js
+++ scripts/services/starredService.js
@@ -1,25 +1,30 @@
 angular
     .module('jeviteca')
     .service('starredService', function(){
+
+        function getArrayKey( type ) {
+            var key = 'favBands';
+            if (type == 'albums')
+                key = 'favAlbums';
+            else if (type == 'genres')
+                key = 'favGenres';
+            return key;
+        }
+
         this.updateStarredValue = function(type, id) {
             if ( typeof( Storage ) !== "undefined" ) {
     			var localStorageValue = document.getElementById( "localStorageValue" );
                 var strObject;
-                var key = 'favBands';
-                if (type == 'albums')
-                    key = 'favAlbums';
-                else if (type == 'genres')
-                    key = 'favGenres';
-                    strObject = localStorage.getItem( key );
+                var key = getArrayKey( type );
+                strObject = localStorage.getItem( key );
                 if ((typeof( strObject ) === "undefined" ) || (strObject ==null))
                     strObject = JSON.stringify([]);
                 var data = JSON.parse(strObject);
-                var index = data.indexOf(id);
-                if (index > -1) {
+                var index = data.indexOf('-'+id+'-');
+                if (index > -1)
                     data.splice(index, 1);
-                } else {
-                    data.push(id);
-                }
+                else
+                    data.push('-'+id+'-');
                 localStorage.setItem(key, JSON.stringify(data));
             }
 		};
@@ -28,20 +33,16 @@ angular
             if ( typeof( Storage ) !== "undefined" ) {
     			var localStorageValue = document.getElementById( "localStorageValue" );
                 var data;
-                var key = 'favBands';
-                if (type == 'albums')
-                    key = 'favAlbums';
-                else if (type == 'genres')
-                    key = 'favGenres';
-                    data = localStorage.getItem( key );
+                var key = getArrayKey( type );
+                data = localStorage.getItem( key );
                 if ((typeof( data ) === "undefined" ) || (data ==null))
                     data = [];
-                var index = data.indexOf(id);
-                if (index > -1) {
+                var index = data.indexOf('-'+id+'-');                
+
+                if (index > -1)
                     return true;
-                } else {
+                else
                     return false;
-                }
 		    }
         };
     });
